407d6f883cb8ab15779bf987db83a4006fae3af2
Olle K <olle@iix3.org>
2013-10-15 08:07:46 +0200

    Cleaned up code a bit, added some docstrings

--- web/myFlaskProject.py
+++ web/myFlaskProject.py
@@ -14,55 +14,59 @@ except:
 
 import data
 
-def static_dir(): return os.path.dirname(os.path.abspath(__file__))
-def tmpl_dir(): return os.path.join(static_dir(), 'templates')
+def static_path(path=False): 
+    """ Returns an absolute path to a file/folder in the project directory """
+    if path:
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+    return os.path.dirname(os.path.abspath(__file__))
 
-def load_data(cache=[None, 0]): 
-    mtime = os.path.getmtime(os.path.join(static_dir(), "data.json"))
+def data_json(cache=[None, 0]): 
+    """ Lazy loading data.json """
+    mtime = os.path.getmtime(static_path("data.json"))
     if mtime != cache[1]:
         cache[1] = mtime
-        cache[0] = data.load(os.path.join(static_dir(), "data.json"))
+        cache[0] = data.load(static_path("data.json"))
     return cache[0]
-def load_main(cache=[None, 0]): 
-    mtime = os.path.getmtime(os.path.join(static_dir(), "main.json"))
+
+def main_json(cache=[None, 0]): 
+    """ Lazy loading main.json """
+    mtime = os.path.getmtime(static_path("main.json"))
     if mtime != cache[1]:
         cache[1] = mtime
-        cache[0] = data.load(os.path.join(static_dir(), "main.json"))
+        cache[0] = data.load(static_path("main.json"))
     return cache[0]
 
 def main():
-    """ Main """
+    """ Main function, only runs when this file is executed """
     def usage(): print("Usage: %s start|stop" % sys.argv[0])
     
     if len(sys.argv) != 2:
         usage()
-        return
 
-    if sys.argv[1] == "start":
+    elif sys.argv[1] == "start":
         print("Starting myFlaskProject")
 
         # Start server on a fork:
         pid = os.fork() 
         if not pid: 
-            sys.stdout = sys.stderr = open(os.path.join(static_dir(), 
-                                            "../log"), 'a')
+            sys.stdout = sys.stderr = open(static_path("../log"), 'a')
             print("Start signal received")
             app.run(host="0.0.0.0")
 
         # Save pid:
-        with open(os.path.join(static_dir(), "../pid"), 'w') as f:
+        with open(static_path("../pid"), 'w') as f:
             print("%d" % pid, file=f)
 
     elif sys.argv[1] == "stop":
         print("Killing myFlaskProject")
-        with open(os.path.join(static_dir(), "../log"), 'a') as f:
+        with open(static_path("../log"), 'a') as f:
             print("Stop signal received", file=f)
 
         # Kill pid in pidfile, then remove it:
         try:
-            with open(os.path.join(static_dir(), "../pid"), 'r') as f:
+            with open(static_path("../pid"), 'r') as f:
                 os.kill(int(f.read().strip()), signal.SIGTERM)
-            os.remove(os.path.join(static_dir(), "../pid"))
+            os.remove(static_path("../pid"))
         except BaseException as e:
             print("Failed to kill process. Reason: %s" % str(e),
                     "If it's still running, please kill it manually",
@@ -70,25 +74,24 @@ def main():
             return
         print("Done")
             
-    else:
-        usage()
-        return
+    else: usage()
 
-app = Flask(__name__, template_folder=tmpl_dir(), static_folder=static_dir())
+app = Flask(__name__, template_folder=static_path("templates"), 
+                        static_folder=static_path())
 
 @app.route("/")
 def main_page():
     """ Returns main page of the website """
-    return render_template("main.html", data=load_data(),
-                            info=load_main())
+    return render_template("main.html", data=data_json(),
+                            info=main_json())
     
 @app.route("/list")
 def project_list():
     """ Returns a list of projects on the website """
-    appdata = load_data()
+    appdata = data_json()
     project_count = data.get_project_count(appdata)
     return render_template("list.html", data=appdata, 
-                            count=project_count, info=load_main())
+                            count=project_count, info=main_json())
     
 @app.route("/techniques")
 def project_tech():
@@ -96,24 +99,24 @@ def project_tech():
     Returns a list of techniques we have used on the website, 
     Each technique also lists projects where the technique was used.
     """
-    techniques = data.get_technique_stats(load_data())
+    techniques = data.get_technique_stats(data_json())
     return render_template("tech.html", techs=techniques, 
-                            info=load_main())
+                            info=main_json())
     
 @app.route("/project/<int:id>")
 def project_single(id):
     """ Returns a page with description for a single project """
-    single_project = data.get_project(load_data(), id)
+    single_project = data.get_project(data_json(), id)
     return render_template("single.html", data=single_project,
-                            info=load_main())
+                            info=main_json())
 
 @app.route("/searchform")
 def search_form():
     """ Returns a page with search form with all the available search options """
-    appdata = load_data()
+    appdata = data_json()
     techniques = data.get_technique_stats(appdata)
     return render_template("searchform.html", data=appdata, 
-                            techs=techniques, info=load_main())
+                            techs=techniques, info=main_json())
     
 @app.route("/search", methods=['POST'])
 def search_results():
@@ -121,10 +124,10 @@ def search_results():
     Sanitizes the search string, 
     counts objects in search results and returns search results page. 
     """
-    appdata = load_data()
+    appdata = data_json()
     sanitized_search = re.sub('[^a-zA-Z0-9\.]', "", request.form['key'])
     techs = request.form.getlist('techfield')
-    technologies = techs if techs else ''
+    technologies = techs if techs else ""
     fields = request.form.getlist('search_field')
     search_fields = fields if fields else None
     sortby = request.form.get('sort_field', 'start_date')
@@ -138,22 +141,22 @@ def search_results():
     return render_template("search.html", data=search_function, 
                             count=results_count, term=sanitized_search, 
                             fields=fields, techs=techs, sort=sort_order, 
-                            sortby=sortby, info=load_main())
+                            sortby=sortby, info=main_json())
     
 @app.errorhandler(404)
 def page_not_found(error):
     """ Page not found """
-    return render_template('404.html', info=load_main()), 404
+    return render_template('404.html', info=main_json()), 404
     
 @app.errorhandler(400)
 def bad_request(error):
     """ Bad request """
-    return render_template('400.html', info=load_main()), 400
+    return render_template('400.html', info=main_json()), 400
     
 @app.errorhandler(500)
 def other(error):
     """ Server error occured """
-    return render_template('error.html', info=load_main()), 500
+    return render_template('error.html', info=main_json()), 500
 
 if __name__ == "__main__":
     main()
